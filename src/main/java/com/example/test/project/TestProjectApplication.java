1afb9a59fa64c4f5531aea8e7b7f856a76b16ecf
snehaNetzone <152726364+snehaNetzone@users.noreply.github.com>
2024-06-02 13:44:21 +0530

    Update TestProjectApplication.java

--- src/main/java/com/example/test/project/TestProjectApplication.java
+++ src/main/java/com/example/test/project/TestProjectApplication.java
@@ -1,19 +1,13 @@
-package com.example.test.project;
+package com.example.springbootdeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestProjectApplication {
+public class SpringbootDeployApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestProjectApplication.class, args); }
-
-	
-        @GetMapping("/string")
-        public String home() {
-            return "Hello World";
-	
+		SpringApplication.run(SpringbootDeployApplication.class, args);
 	}
 
 }
