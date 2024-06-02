052dcf6aaac6d2180fe22df34f6930e7400c96f9
snehaNetzone <152726364+snehaNetzone@users.noreply.github.com>
2024-06-02 14:59:21 +0530

    Update TestProjectApplication.java

--- src/main/java/com/example/test/project/TestProjectApplication.java
+++ src/main/java/com/example/test/project/TestProjectApplication.java
@@ -13,21 +13,4 @@ public class DemoApplication {
 }
 
 
-package com.example.springbootdeploy;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloController {
-
-       @RequestMapping("/hello")
-       public String hello(){
-            return "Hello World";
-           
-           }
- 
-
-}
-
-          
